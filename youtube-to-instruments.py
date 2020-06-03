41fb9e268b3bac7efb24299679a59c6f636926e3
emmanuel <ejariza@gmail.com>
2020-06-03 13:27:44 -0500

    Added parameter filepath to separate instruments

--- youtube-to-instruments.py
+++ youtube-to-instruments.py
@@ -1,12 +1,12 @@
 """Naval Fate.
 
 Usage:
-  youtube-to-instruments.py --name <name> --url <youtube-url>
+  youtube-to-instruments.py --name <name> --url <youtube-url> [--basepath <basepath>]
 
 Options:
-  --name name of the song
-  --url url of the youtube video.
-
+  --name        name of the song.
+  --url         url of the youtube video.
+  --basepath    basepath for extraction.
 """
 from docopt import docopt
 import os
@@ -15,12 +15,15 @@ arguments = docopt(__doc__, version='Youtube to instruments 0.0.1')
 
 
 if __name__ == '__main__':
-    name = arguments["--name"]
-    url = arguments["--url"]
+    name = arguments["<name>"]
+    url = arguments["<youtube-url>"]
+    basepath = os.path.abspath(arguments["<basepath>"] or os.getcwd())
+    path = os.path.join(basepath, name)
+    instruments_path = os.path.join(path, "instruments")
+    wav_path = os.path.join(path, name + ".wav")
 
-    os.system("mkdir -p {name}/instruments".format(name = name))
-    os.system("youtube-dl --extract-audio --audio-format wav -o {name}/{name}.wav {url}".format(name = name, url = url))
-    os.system("spleeter separate -i {name}/{name}.wav -p spleeter:5stems -o {name}/instruments".format(name = name))
-
-
-    print(arguments)
+    os.system("mkdir -p {instruments_path}".format( instruments_path = instruments_path ))
+    os.system("youtube-dl --extract-audio --audio-format wav -o {wav_path} {url}".format(wav_path = wav_path, url = url))
+    os.system("spleeter separate -i {wav_path} -p spleeter:5stems -o {instruments_path}".format(instruments_path = instruments_path , wav_path = wav_path))
+    os.system("mv {instruments_path}/{name}/*.wav {instruments_path}".format(instruments_path = instruments_path, name = name))
+    os.system("rm -rf {instruments_path}/{name}/".format(instruments_path = instruments_path, name = name))
